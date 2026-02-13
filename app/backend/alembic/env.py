739b503474b98f1f0cae9573810c18f32db5e9a4
Oledjeee69 <olegaunderground@gmail.com>
2026-02-13 19:15:14 +0300

    env.py

--- app/backend/alembic/env.py
+++ app/backend/alembic/env.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 from app.db import Base  # noqa: F401
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", "postgresql://wishl_user:L2ij0MwhO6VYCFHc43oIjbUbdNkoJbQw@dpg-d67689juibrs73cem9mg-a.oregon-postgres.render.com/wishl")
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
